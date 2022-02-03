811f5ae95452feef9893a85924084d848ccd711f
promisszn <olayinkapromise80@gmail.com>
2022-02-03 07:58:23 +0100

    fixed reload issue

--- src/main.js
+++ src/main.js
@@ -5,9 +5,4 @@ import store from "./store";
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 
-let app;
-firebase.auth().onAuthStateChanged(() => {
-  if (!app) {
-    createApp(App).use(store).use(router).mount("#app");
-  }
-});
+createApp(App).use(store).use(router).mount('#app');
